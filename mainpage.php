9d408c86e9ea1d2f0bab57d0339dd7a15d0aab57
alee012 <120870188+alee012@users.noreply.github.com>
2024-11-21 03:02:47 -0500

    Updated topnav

--- mainpage.php
+++ mainpage.php
@@ -11,14 +11,32 @@ session_start();
     <body>
         <header>
             <div class="topnav">
-                <a href="mainpage.php">Home</a>
-                <a href="Restaurantview.php">Restaurant View</a>
-                <form action="phpscripts/search.php" method="POST"><input id="searchField" name="searchField" type="text" placeholder="Search..."></form>
-                <a href="accountPage.php"><img src="images/profileOutline.png" height="50" alt="profile outline"></a>
-                <a href="cart.php">Cart</a>
-                <img src="images/FoodFlashLogo.png" height="50">
-                <a href="searchPage.php" style="float: right;"><img src="images/magnifying-glass.png" height="50px" alt="magnifying glass"></a>
+
+                <!-- FoodFlash logo -->
+                <img src="images/FoodFlashLogo.png" class="logo" alt="FoodFlash Logo">
+
+                
+
+                <!-- Navigation links -->
+                <div class="nav-links">
+                    <a href="mainpage.php">Home</a>
+                    <a href="Restaurantview.php">Restaurant View</a>
+                    <a href="accountPage.php"><img src="images/profileOutline.png" height="50" alt="profile outline"></a>
+                    <a href="cart.php">Cart</a>
+                    <a href="bookmarkedRestaurants.php">My Bookmarks</a>
+                </div>
+
+                 <!-- Search box -->
+                 <form action="phpscripts/search.php" method="POST">
+                    <input id="searchField" name="searchField" type="text" placeholder="Search...">
+                </form>
+                <a href="searchPage.php" style="float: right;">
+                    <img src="images/magnifying-glass.png" height="50px" alt="Search Icon">
+                </a>
+
             </div>
+
+  
         </header>
         <div id="category_section">
             <p class="headers">Categories</p>
